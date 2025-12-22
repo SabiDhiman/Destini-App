29ddc1d8df97b32495d28720260ec3f0af83070a
Sabi Dhiman <sabi98@live.co.uk>
2025-12-22 22:48:25 +0000

    setup view constraints and outlets

---
+++ Destini-MVC/Destini-MVC/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice1Button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
