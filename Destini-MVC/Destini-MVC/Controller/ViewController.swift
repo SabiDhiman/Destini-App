b60f4df8a58fd1396459371d860d8ae289f38ae8
Sabi Dhiman <sabi98@live.co.uk>
2025-12-22 23:19:05 +0000

    added models and updated vc

--- Destini-MVC/Destini-MVC/Controller/ViewController.swift
+++ Destini-MVC/Destini-MVC/Controller/ViewController.swift
@@ -2,20 +2,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice2Button: UIButton!
     @IBOutlet weak var choice1Button: UIButton!
     
-    
+    let storyBrain = StoryBrain()
+    let storyNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.stories[storyNumber].title
+        choice1Button.setTitle(storyBrain.stories[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyNumber].choice2, for: .normal)
     }
     
 }
---
+++ Destini-MVC/Destini-MVC/Model/StoryBrain.swift
@@ -0,0 +1,15 @@
+
+
+struct StoryBrain {
+    let stories = [
+        Story(title: "you see a fork on the road",
+                          choice1: "take left",
+                          choice2: "take right"),
+        Story(title: "you see a tiger",
+                          choice1: "shout for help",
+                          choice2: "play dead"),
+        Story(title: "you find a tresure chest",
+                          choice1: "open it",
+                          choice2: "check for traps")
+    ]
+}
